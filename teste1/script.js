b282b11f1c6f9fbe1b545a028f231d8c15d09bf6
José Leandro Vilela <joseleandro2807@gmail.com>
2024-03-18 19:05:37 -0300

    testes

---
+++ teste1/script.js
@@ -0,0 +1,35 @@
+
+    function imgSlider(anything){
+       const jogoDaVelha = document.querySelector('#jogoDaVelha');
+       const campoMinado = document.querySelector('.campoMinado');
+       const adivinhacao = document.querySelector('.adivinhacao');
+         
+
+       const slider = document.querySelector('.starbucks');
+       slider.style.transition = 'all 1s ease-in-out';
+       slider.src = anything;
+
+        if(anything == 'images/img2.png'){
+            adivinhacao.style.display = 'none';
+            jogoDaVelha.style.display = 'none';
+            campoMinado.style.display = 'block';
+        }else if(anything == 'images/img3.png'){
+            jogoDaVelha.style.display = 'none';
+            campoMinado.style.display = 'none';
+            adivinhacao.style.display = 'block';
+        }else{
+            campoMinado.style.display = 'none';
+            adivinhacao.style.display = 'none';
+            jogoDaVelha.style.display = 'block';
+        }
+
+    }
+
+    function changeCircleColor(color){
+        const circle = document.querySelector('.circle');
+        circle.style.background = color;
+    }
+
+  
+
+
