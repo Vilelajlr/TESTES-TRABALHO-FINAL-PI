948f42e8a65093c9d3aeae5331348ca67e1a7b09
José Leandro Vilela <joseleandro2807@gmail.com>
2024-03-19 10:35:09 -0300

    att

--- teste1/script.js
+++ teste1/script.js
@@ -31,5 +31,11 @@
     }
 
   
+    const menuToggle = document.querySelector('.menu-toggle');
+    const menu = document.querySelector('.menu');
 
+    menuToggle.addEventListener('click', function(){
+        menu.classList.toggle('active');
+        menuToggle.classList.toggle('active')
+    });
 
